ecf9c44fd95dae945eb90c2e71a6809229735328
Your Name <you@example.com>
2025-06-16 17:33:26 +0300

    final commitv1

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({ 
+   
+  plugins: [react()],
   base: '/Bota-Andrei-Proiecte-PMIS-CFSS',
 
-}));
+});
