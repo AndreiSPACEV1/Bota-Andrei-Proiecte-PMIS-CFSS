4dab7ecfd90d791ec8b797e8e73598fb7f7414a4
Your Name <you@example.com>
2025-06-16 17:24:29 +0300

    final commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/Bota-Andrei-Proiecte-PMIS-CFSS/',
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: '/Bota-Andrei-Proiecte-PMIS-CFSS',
+
 }));
